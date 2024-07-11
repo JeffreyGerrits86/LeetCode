9ccccbc05f09e6e608d20737a2c8e87d95b0c428
Jeffrey Gerrits <jeffreygerrits86@gmail.com>
2024-07-11 20:57:31 +0200

    Initial commit

---
+++ Insert Delete GetRandom/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Insert_Delete_GetRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomizedSet randomizedSet = new RandomizedSet();
+            bool entered = randomizedSet.Insert(9);
+            bool removed = randomizedSet.Remove(10);
+            entered = randomizedSet.Insert(5);
+            entered = randomizedSet.Insert(1);
+            entered = randomizedSet.Insert(12);
+            entered = randomizedSet.Insert(45);
+            entered = randomizedSet.Insert(8);
+            entered = randomizedSet.Insert(7);
+            entered = randomizedSet.Insert(5);
+
+            Console.WriteLine(entered);
+            foreach(var item in randomizedSet._set)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+    public class RandomizedSet
+    {
+        public HashSet<int> _set { get; set; }
+
+        public RandomizedSet()
+        {
+            this._set = new HashSet<int>();
+        }
+
+        public bool Insert(int val)
+        {
+            if (_set.Add(val))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(int val)
+        {
+            if (_set.Remove(val))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetRandom()
+        {
+            Random random = new Random();
+            int size = _set.Count();
+            return _set.ElementAt(random.Next(0, size));
+        }
+    }
+}
---
+++ NumberOfIslands/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace NumberOfIslands
+{
+    internal class Program
+    {
+        static char[][] grid = new char[][]
+        {
+            new char[] {'1', '1', '0', '0', '0'},
+            new char[] {'1', '1', '0', '0', '0'},
+            new char[] {'0', '0', '1', '0', '0'},
+            new char[] {'0', '0', '0', '1', '1'}
+        };
+        // necessary items
+        static int m = grid.Length;
+        static int n = grid[0].Length;
+        static int result = 0;
+
+
+        static void Main(string[] args)
+        {
+            int result = NumIslands(grid);
+            Console.WriteLine(result);
+        }
+
+        static int NumIslands(char[][] grid)
+        {
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {                   
+                    if (grid[i][j] == '1')
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            result++;
+                        }else if(i > 0 && j == 0)
+                        {
+                            if (grid[i - 1][j] == '0')
+                                result++;
+                        }else if(i == 0 && j > 0)
+                        {
+                            if (grid[i][j - 1] == '0')
+                                result++;
+                        }
+                        else
+                        {
+                            if (grid[i][j - 1] == '0' && grid[i - 1][j] == '0')
+                                result++;
+                        }
+                    }                    
+                }
+            }
+            return result;
+        }
+    }
+}
